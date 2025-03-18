fa47cb65228022afbab2ef55af7efdfd074b3f4d
SahilPanhotra <sahilpanhotra@gmail.com>
2025-03-18 17:25:12 +0530

    feat: Add Features component to enhance the home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
+import Features from "./components/Features";
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <NavBar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 }
