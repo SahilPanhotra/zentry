58cb65ed0d1bb4cc8d48848724a8b4b4ad0fe52c
SahilPanhotra <sahilpanhotra@gmail.com>
2025-03-18 16:52:16 +0530

    chore:Add About and AnimatedTitle Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "./components/Hero";
-
+import About from "./components/About";
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <About />
     </main>
   );
 }
