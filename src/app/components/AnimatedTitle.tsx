f9e95ae31be6e95bde6cc99dc0156b6c1e9abba7
SahilPanhotra <sahilpanhotra@gmail.com>
2025-03-18 17:09:13 +0530

    chore: Add react-use dependency; enhance page layout with NavBar component

--- src/app/components/AnimatedTitle.tsx
+++ src/app/components/AnimatedTitle.tsx
@@ -10,29 +10,41 @@ const AnimatedTitle = ({ title, containerClass }: { title: string, containerClas
   const containerRef = useRef(null);
 
   useEffect(() => {
+    // First, we create a special box (context) where our animation will live
     const ctx = gsap.context(() => {
+      // We're making a timeline - it's like a storyboard for our animation!
       const titleAnimation = gsap.timeline({
         scrollTrigger: {
+          // We tell the animation to start when our container comes into view
           trigger: containerRef.current,
+          // Start when the bottom of the screen hits 100px from the top of our container
           start: "100 bottom",
+          // End when the center of our container hits the bottom of the screen
           end: "center bottom",
+          // This tells the animation what to do:
+          // - "play" when it starts
+          // - "none" if we scroll forward
+          // - "none" if we scroll backward
+          // - "reverse" when we scroll back up
           toggleActions: "play none none reverse",
         },
       });
 
+      // Now we tell each word how to appear!
       titleAnimation.to(
-        ".animated-word",
+        ".animated-word", // We're animating all the words with this class
         {
-          opacity: 1,
-          transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
-          ease: "power2.inOut",
-          stagger: 0.02,
+          opacity: 1,                   // Make the words visible (they start invisible)
+          transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)", // Move words to their final position
+          ease: "power2.inOut",         // Make the movement smooth, like a bouncy ball
+          stagger: 0.02,                // Wait 0.02 seconds between each word's animation
         },
-        0
+        0  // Start right away (0 seconds delay)
       );
     }, containerRef);
 
-    return () => ctx.revert(); // Clean up on unmount
+    // When we're done, clean up our animation box so it doesn't make a mess
+    return () => ctx.revert();
   }, []);
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
+import NavBar from "./components/NavBar";
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
       <Hero />
       <About />
     </main>
