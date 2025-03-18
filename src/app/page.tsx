b24df23f1d2ee76a8f72f9542c691aa071629ecd
SahilPanhotra <sahilpanhotra@gmail.com>
2025-03-18 17:46:42 +0530

    feat: Add Contact component to the home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Features from "./components/Features";
 import Story from "./components/Story";
+import Contact from "./components/Contact";
+
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
@@ -11,6 +13,7 @@ export default function Home() {
       <About />
       <Features />
       <Story />
+      <Contact />
     </main>
   );
 }
